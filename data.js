5e5f6453888dd5db499bd351b8749b5086c0b6bb
R.M Heshan Sathyanga <heshansathyanga.hs@gmail.com>
2025-08-20 17:36:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         id: 'proj1',
-        img: 'projects/Screenshot 2025-08-06 110237.png',
+        img: 'Screenshot 2025-08-06 110237.png',
         title: 'Game Download Website',
         tags: ['HTML', 'CSS', 'JavaScript', 'PHP'],
         description: 'Download amazing games directly via Steam completely FREE and ready to play!',
@@ -10,7 +10,7 @@ const projects = [
     },
     {
         id: 'proj2',
-        img: 'projects/Screenshot 2025-08-06 110641.png',
+        img: 'Screenshot 2025-08-06 110641.png',
         title: 'Movie Download Website',
         tags: ['HTML', 'CSS', 'JavaScript', 'PHP'],
         description: 'Download amazing movies directly via Netflix completely FREE and ready to watch!',
@@ -19,7 +19,7 @@ const projects = [
     },
     {
         id: 'proj3',
-        img: 'projects/project-3.jpg',
+        img: 'project-3.jpg',
         title: 'Weather App',
         tags: ['API', 'JavaScript', 'HTML', 'CSS'],
         description: 'A simple and intuitive weather application that provides real-time weather data for any location using a third-party API.',
@@ -32,55 +32,55 @@ const certificateData = {
     cert1: {
         title: 'UWU EXPO 2022 - Exhibition',
         issuer: 'UWU EXPO',
-        img: 'cetificates/IMG_20250806_113154_942.jpg',
+        img: 'IMG_20250806_113154_942.jpg',
         downloadLink: 'cetificates/IMG_20250806_113154_942.jpg'
     },
     cert2: {
         title: 'Web Application Development',
         issuer: 'Ministry of Education',
-        img: 'cetificates/IMG_20250806_113248_278.jpg',
+        img: 'IMG_20250806_113248_278.jpg',
         downloadLink: 'cetificates/IMG_20250806_113248_278.jpg'
     },
     cert3: {
         title: 'ALL ISLAND SCHOOL ROBOT AND INVENTION COMPETITION',
         issuer: 'Ministry of Education',
-        img: 'cetificates/IMG_20250806_113227_017.jpg',
+        img: 'IMG_20250806_113227_017.jpg',
         downloadLink: 'cetificates/IMG_20250806_113227_017.jpg'
     },
     cert4: {
         title: 'ALL ISLAND SCHOOL ROBOT COMPETITION',
         issuer: 'Ministry of Education',
-        img: 'cetificates/IMG_20250806_113310_893.jpg',
+        img: 'IMG_20250806_113310_893.jpg',
         downloadLink: 'cetificates/IMG_20250806_113310_893.jpg'
     },
     cert5: {
         title: 'Dhamma School  Exam ',
         issuer: 'Dhamma School',
-        img: 'cetificates/IMG_20250806_113404_606.jpg',
+        img: 'IMG_20250806_113404_606.jpg',
         downloadLink: 'cetificates/IMG_20250806_113404_606.jpg'
     },
     cert6: {
         title: 'ESOFT METRO CAMPUS is a Pearson Assured organisation',
         issuer: 'ESOFT METRO CAMPUS',
-        img: 'cetificates/IMG_20250806_113437_266.jpg',
+        img: 'IMG_20250806_113437_266.jpg',
         downloadLink: 'cetificates/IMG_20250806_113437_266.jpg'
     },
     cert7: {
         title: 'All Island School Drawing Competition',
         issuer: 'All Island School',
-        img: 'cetificates/IMG_20250806_113502_271.jpg',
+        img: 'IMG_20250806_113502_271.jpg',
         downloadLink: 'cetificates/IMG_20250806_113502_271.jpg'
     },
     cert8: {
         title: 'Certificate of Grade 5 Scholarship Examination',
         issuer: 'DEPARTMENT OF EXAMINATIONS',
-        img: 'cetificates/IMG_20250806_113328_879.jpg',
+        img: 'IMG_20250806_113328_879.jpg',
         downloadLink: 'cetificates/IMG_20250806_113328_879.jpg'
     },
     cert9: {
         title: 'ART COMPETITION',
         issuer: 'Se Ian Bank PLC',
-        img: 'cetificates/IMG_20250806_113421_212.jpg',
+        img: 'IMG_20250806_113421_212.jpg',
         downloadLink: 'cetificates/IMG_20250806_113421_212.jpg'
     }
 };
